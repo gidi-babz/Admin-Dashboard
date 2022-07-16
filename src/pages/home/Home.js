40b1413d42f84df0db1d031cbffcdf4c2ee4118c
Gidi Babz <tunex4ind@gmail.com>
2022-07-16 18:13:19 +0100

    commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,7 @@
 import './home.scss';
 
 const Home = () => {
-	return (
-		<div className="home">
-			<h1 className="title">Home</h1>
-		</div>
-	);
+	return <div className="home">Home</div>;
 };
 
 export default Home;
